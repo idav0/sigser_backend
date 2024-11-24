9105f0b86fdcc3c2243194e7101c3f23f1055ca1
idav0 <20193tn142@utez.edu.mx>
2024-11-24 09:09:19 -0600

    BASE DATA creation script added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -20,6 +20,6 @@ INSERT INTO device_types (id, name) SELECT 5, 'SMARTWATCH' WHERE NOT EXISTS (SEL
 INSERT INTO device_types (id, name) SELECT 6, 'VIDEOGAME_CONSOLE' WHERE NOT EXISTS (SELECT 1 FROM device_types WHERE id = 6);
 INSERT INTO device_types (id, name) SELECT 7, 'SPEAKER' WHERE NOT EXISTS (SELECT 1 FROM device_types WHERE id = 7);
 
-INSERT INTO users (id, email, name, lastname, phone, password, user_type_id) SELECT 1, 'superadmin', 'SUPER', 'ADMIN', '0000000000', '$2a$10$ofdE1nUvF0GJh7kkISNBdeYM2WNX/uGWF30UgLXxl.PGo4n0gvZU.', 1 WHERE NOT EXISTS (SELECT 1 FROM users WHERE id = 1);
+INSERT INTO users (id, email, name, lastname, phone, password, active, new_user, user_type_id) SELECT 1, 'superadmin@sigser.com', 'SUPER', 'ADMIN', '0000000000', '$2a$10$ofdE1nUvF0GJh7kkISNBdeYM2WNX/uGWF30UgLXxl.PGo4n0gvZU.', TRUE, TRUE, 1 WHERE NOT EXISTS (SELECT 1 FROM users WHERE id = 1);
 
 
